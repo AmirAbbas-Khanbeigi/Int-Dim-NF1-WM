1ad627aba964538ca20377a1da464ae557123df6
AmirAbbas-Khanbeigi <134785813+AmirAbbas-Khanbeigi@users.noreply.github.com>
2024-12-07 12:38:33 +0000

    new version of time series extractor function was made

--- dataImportFuctions.py
+++ dataImportFuctions.py
@@ -59,3 +59,189 @@ def main_data_dic_generator(base_dir,
             print(f"{key}: shape = {dic[key].shape}")
 
     return dic
+
+
+def TS_extractor(main_data_dic,
+                    index_list, 
+                    print_bool=False):
+
+    # Dictionary to store arrays
+    dic_2back_TSs = {}
+    dic_0back_TSs = {}
+    dic_entire_TSs = {}
+    #group_size = 12
+    # total_rows = 144
+    # selected_rows = [i for i in range(total_rows) if (i // group_size) % 2 != 0]
+
+    for i in index_list:  # Range from 001 to 020
+        
+        TS_2back= []
+        TS_0back= []
+        TS_entire= []
+
+        keyTS = f'{i:03d}'  
+        if print_bool==True:
+            print(f"main_data_dic[{keyTS}].shape= ", main_data_dic[keyTS].shape,
+                  " with the key as ", keyTS)
+        
+        N_parcels= main_data_dic[keyTS].shape[1]
+        for j in range(6):
+            # if print_bool==True:  
+            #     print(f"{j+1}-th one minute:")
+            #key_12TimePoint_FCs= keyTS + f'{j+1}'
+
+            #2-back FCs
+            rows_2back= [12*(2*j+1)+k for k in range(12)] 
+            # if print_bool==True:  
+            #     print("rows_2back= ", rows_2back)
+            
+            twoback_TS_30secs= main_data_dic[keyTS][rows_2back, :]
+            # if print_bool==True:
+            #     print("twoback_TS_30secs.shape= ", twoback_TS_30secs.shape)
+            #     #print("twoback_TS_30secs= ", twoback_TS_30secs)
+            
+            TS_2back.append(twoback_TS_30secs)
+            #print("dic_AAL3_controls_2back_FCs[key_0_2back_FCs]= ", dic_AAL3_controls_2back_FCs[key_0_2back_FCs])
+            # if print_bool==True:
+            #     print(f"dic_2back_FCs[{key_12TimePoint_FCs}].shape= ",
+            #           dic_2back_FCs[key_12TimePoint_FCs].shape)
+
+            #0-back FCs
+            rows_0back= [12*(2*j)+k for k in range(12)]
+            # if print_bool==True:  
+            #     print("0back_rows= ", rows_0back)
+            # zeroback_TS_30secs= np.zeros((12,166))
+
+            zeroback_TS_30secs = main_data_dic[keyTS][rows_0back, :]
+            # if print_bool==True:
+                # print("zeroback_TS_30secs.shape= ", zeroback_TS_30secs.shape)
+                # #print("zeroback_TS_30secs= ", zeroback_TS_30secs)
+            #print("dic_AAL3_controls_0back_FCs[key_0_2back_FCs]= ", dic_AAL3_controls_0back_FCs[key_0_2back_FCs])
+
+            # if print_bool==True:
+            #     print(f"dic_0back_FCs[{key_12TimePoint_FCs}].shape= ",
+            #         dic_0back_FCs[key_12TimePoint_FCs].shape)
+
+            TS_0back.append(zeroback_TS_30secs)
+
+        #entire
+        TS_entire= main_data_dic[keyTS]
+
+        TS_2back= np.array(TS_2back)
+        TS_0back= np.array(TS_0back)
+        TS_entire= np.array(TS_entire)
+            
+        TS_2back = TS_2back.reshape(-1, N_parcels)
+        TS_0back = TS_0back.reshape(-1, N_parcels)
+
+        dic_2back_TSs[keyTS]= TS_2back
+        dic_0back_TSs[keyTS]= TS_0back
+        dic_entire_TSs[keyTS]= TS_entire
+
+
+
+            #2 minus 0 back FCs
+            #print("dic_AAL3_controls_2minus0_FCs[key_0_2back_FCs]= ", dic_AAL3_controls_2minus0_FCs[key_0_2back_FCs])
+            # if print_bool==True:
+            #     print(f"dic_2minus0_FCs[{key_12TimePoint_FCs}].shape= ",
+            #         dic_2minus0_FCs[key_12TimePoint_FCs].shape)
+    return  dic_2back_TSs, dic_0back_TSs, dic_entire_TSs
+            
+        #print(np.shape(dic_AAL3_controls[key]))
+
+    # for key in sorted(dic_AAL3_controls_2minus0_FCs.keys()):
+    #     print(f"{key}: shape = {dic_AAL3_controls_2minus0_FCs[key].shape}")
+    #     FC_matrix_dfs_plot([dic_AAL3_controls_2minus0_FCs[key]])
+
+def TS_extractor_v2(main_data_dic,
+                    index_list, 
+                    print_bool=False):
+
+    # Dictionary to store arrays
+    dic_2back_TSs = {}
+    dic_0back_TSs = {}
+    dic_entire_TSs = {}
+    #group_size = 12
+    # total_rows = 144
+    # selected_rows = [i for i in range(total_rows) if (i // group_size) % 2 != 0]
+
+    for key in main_data_dic.keys():  # Range from 001 to 020
+        
+        TS_2back= []
+        TS_0back= []
+        TS_entire= []
+
+        # keyTS = f'{i:03d}' 
+        keyTS = key 
+        if print_bool==True:
+            print(f"main_data_dic[{keyTS}].shape= ", main_data_dic[keyTS].shape,
+                  " with the key as ", keyTS)
+        
+        N_parcels= main_data_dic[keyTS].shape[1]
+        for j in range(6):
+            # if print_bool==True:  
+            #     print(f"{j+1}-th one minute:")
+            #key_12TimePoint_FCs= keyTS + f'{j+1}'
+
+            #2-back FCs
+            rows_2back= [12*(2*j+1)+k for k in range(12)] 
+            # if print_bool==True:  
+            #     print("rows_2back= ", rows_2back)
+            
+            twoback_TS_30secs= main_data_dic[keyTS][rows_2back, :]
+            # if print_bool==True:
+            #     print("twoback_TS_30secs.shape= ", twoback_TS_30secs.shape)
+            #     #print("twoback_TS_30secs= ", twoback_TS_30secs)
+            
+            TS_2back.append(twoback_TS_30secs)
+            #print("dic_AAL3_controls_2back_FCs[key_0_2back_FCs]= ", dic_AAL3_controls_2back_FCs[key_0_2back_FCs])
+            # if print_bool==True:
+            #     print(f"dic_2back_FCs[{key_12TimePoint_FCs}].shape= ",
+            #           dic_2back_FCs[key_12TimePoint_FCs].shape)
+
+            #0-back FCs
+            rows_0back= [12*(2*j)+k for k in range(12)]
+            # if print_bool==True:  
+            #     print("0back_rows= ", rows_0back)
+            # zeroback_TS_30secs= np.zeros((12,166))
+
+            zeroback_TS_30secs = main_data_dic[keyTS][rows_0back, :]
+            # if print_bool==True:
+                # print("zeroback_TS_30secs.shape= ", zeroback_TS_30secs.shape)
+                # #print("zeroback_TS_30secs= ", zeroback_TS_30secs)
+            #print("dic_AAL3_controls_0back_FCs[key_0_2back_FCs]= ", dic_AAL3_controls_0back_FCs[key_0_2back_FCs])
+
+            # if print_bool==True:
+            #     print(f"dic_0back_FCs[{key_12TimePoint_FCs}].shape= ",
+            #         dic_0back_FCs[key_12TimePoint_FCs].shape)
+
+            TS_0back.append(zeroback_TS_30secs)
+
+        #entire
+        TS_entire= main_data_dic[keyTS]
+
+        TS_2back= np.array(TS_2back)
+        TS_0back= np.array(TS_0back)
+        TS_entire= np.array(TS_entire)
+            
+        TS_2back = TS_2back.reshape(-1, N_parcels)
+        TS_0back = TS_0back.reshape(-1, N_parcels)
+
+        dic_2back_TSs[keyTS]= TS_2back
+        dic_0back_TSs[keyTS]= TS_0back
+        dic_entire_TSs[keyTS]= TS_entire
+
+
+
+            #2 minus 0 back FCs
+            #print("dic_AAL3_controls_2minus0_FCs[key_0_2back_FCs]= ", dic_AAL3_controls_2minus0_FCs[key_0_2back_FCs])
+            # if print_bool==True:
+            #     print(f"dic_2minus0_FCs[{key_12TimePoint_FCs}].shape= ",
+            #         dic_2minus0_FCs[key_12TimePoint_FCs].shape)
+    return  dic_2back_TSs, dic_0back_TSs, dic_entire_TSs
+            
+        #print(np.shape(dic_AAL3_controls[key]))
+
+    # for key in sorted(dic_AAL3_controls_2minus0_FCs.keys()):
+    #     print(f"{key}: shape = {dic_AAL3_controls_2minus0_FCs[key].shape}")
+    #     FC_matrix_dfs_plot([dic_AAL3_controls_2minus0_FCs[key]])
